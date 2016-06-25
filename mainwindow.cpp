8b6ff6bbf5dde0386d1701106680d23fb3411eb7
Anselmo L. S. Melo <anselmolsm@gmail.com>
2016-06-25 09:20:20 -0300

    Using Stacked Widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,23 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
